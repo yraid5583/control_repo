49c2f8e5dd304c73b69f06a7f3804b2617a14e03
yraid5583 <103963435+yraid5583@users.noreply.github.com>
2022-04-19 21:06:12 +0300

    Update site.pp

--- mainfests/site.pp
+++ mainfests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file {'/tmp/readme':
-    ensure  => file,
-    content => 'This is a read me, Yousif.',
-    owner   => 'root',
-    notify  => Service[sshd]
-  }
-  service { sshd: 
-    ensure  => running,
-  }
+  include nginx
 }
